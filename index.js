250322931b0f47fe7d6b4b34a413b2984d39d77a
Joe Tustin <j.tustin@gmail.com>
2019-02-21 21:30:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function currentLine(katzDeliLine) {
   for (var i = 0; i < numberInLine; i++) {
       currentLine.push(` ${i+1}. ${katzDeliLine[i]}`)
     console.log(currentLine)
-    console.log(numberHistory)
+    console.log(historyLine)
       }
   return "The line is currently:" + currentLine
 }
